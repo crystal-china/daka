7f65eda9c7400b9ae0c7eb6235b993e59adfd1ff
RetiredGuitar64 <spider.yuxuan@gmail.com>
2025-01-31 20:38:03 +0800

    format auth.cr

--- src/daka/auth.cr
+++ src/daka/auth.cr
@@ -2,7 +2,6 @@ require "kemal-basic-auth"
 
 class CustomAuthHandler < Kemal::BasicAuth::Handler
   only ["/admin"]
-
   def call(context)
     return call_next(context) unless only_match?(context)
     super
