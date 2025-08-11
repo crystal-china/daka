1125f99a48f0804575791f9c77a215a38225e090
Billy.Zheng <vil963@gmail.com>
2025-08-11 12:04:45 +0800

    Fix crystal tool format

--- src/daka/auth.cr
+++ src/daka/auth.cr
@@ -2,6 +2,7 @@ require "kemal-basic-auth"
 
 class CustomAuthHandler < Kemal::BasicAuth::Handler
   only ["/admin"]
+
   def call(context)
     return call_next(context) unless only_match?(context)
     super
